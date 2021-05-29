8f782f25872d57f0d21b12d22066fa3b10d707c2
Ayman <84075654+ayman-musk@users.noreply.github.com>
2021-05-30 00:21:13 +0530

    Heron's formula
    Heron's formula

---
+++ herons.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int a ,b ,c,s;
+float h;
+
+void main()
+{printf("enter the sides of the triangle :");
+scanf("%d%d%d",&a,&b,&c);
+s = (a+b+c)/2;
+h = sqrt(s*(s-a)*(s-b)*(s-c));
+printf("The area of triangle is =%f \n",h);
+}
+
