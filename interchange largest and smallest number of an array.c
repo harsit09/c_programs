5706df68c9416d2964307a181fb5edaf4038c5de
Ayman <84075654+ayman-musk@users.noreply.github.com>
2021-06-10 12:06:18 +0530

    interchange largest and smallest number of an array
    interchange largest and smallest number of an array

---
+++ interchange largest and smallest number of an array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int a[50],largest,smallest,n,swapper,i;
+printf("Enter the number of elements in the array:\n");
+scanf("%d",&n);
+printf("Enter the elements of the array: \n");
+for(i=0;i<n;i++)
+{scanf("%d",&a[i]);}
+largest=0;
+smallest=0;
+for(i=1;i<n;i++)
+{if(a[i]>a[largest])
+{largest=i;}
+if(a[i]<a[smallest])
+{smallest=i;}
+}
+printf("\n Before interchange: \n Largest number = %d \n Smallest number = %d",a[largest],a[smallest]);
+swapper=a[largest];
+a[largest]=a[smallest];
+a[smallest]=swapper;
+printf("\n After interchange: \n Largest number = %d \n Smallest number = %d",a[largest],a[smallest]);
+}
