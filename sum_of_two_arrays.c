05decee7314f2e99227c190b2213ebcaadf0eb2d
Ayman <84075654+ayman-musk@users.noreply.github.com>
2021-06-17 00:51:49 +0530

    Update sum_of_two_arrays.c

--- sum_of_two_arrays.c
+++ sum_of_two_arrays.c
@@ -5,16 +5,16 @@ int a[50][50],b[50][50],sum[50][50],rows1,rows2,cols1,cols2,i,j;
 printf("Enter the number of rows and columns of array 1: \n");
 scanf("%d%d",&rows1,&cols1);
 for(i=0;i<rows1;i++)
-{printf("Enter the elements of row %d ; \n",(i+1));
+{printf("Enter the elements of row %d: \n",(i+1));
 for(j=0;j<cols1;j++)
 {scanf("%d",&a[i][j]);}
 }
 printf("Enter the number of rows and columns of array 2: \n");
 scanf("%d%d",&rows2,&cols2);
 for(i=0;i<rows2;i++)
-{printf("Enter the elements of row %d ; \n",(i+1));
+{printf("Enter the elements of row %d : \n",(i+1));
 for(j=0;j<cols2;j++)
-{scanf("%d",&a[i][j]);}
+{scanf("%d",&b[i][j]);}
 }
 if(rows1!=rows2 && cols1!=cols2)
 {printf("The two matrices need to have equal number of rows and columns to add! \n");
@@ -22,8 +22,10 @@ exit(0);}
 else
 {
 for(i=0;i<rows1;i++)
-{for(j=0;j<cols1;j++)
-{sum[i][j]=a[i][j]+b[i][j];}
+{ sum[i][j]=0;
+for(j=0;j<cols1;j++)
+{sum[i][j]= a[i][j]+b[i][j];}
+}
 printf("The sum of the two arrays is as follows: \n");
 for(i=0;i<rows1;i++)
 {for(j=0;j<cols1;j++)
@@ -32,4 +34,4 @@ for(i=0;i<rows1;i++)
 printf("\n");}
 }
 }
-}
+
