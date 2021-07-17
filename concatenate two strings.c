fa8c23996dc9ad59973dac00990d539eeacd75b6
Ayman <84075654+ayman-musk@users.noreply.github.com>
2021-07-17 09:24:44 +0530

    Update concatenate two strings.c

--- concatenate two strings.c
+++ concatenate two strings.c
@@ -2,20 +2,30 @@
 #include<string.h>
 void main()
 {
-char s1[100],s2[100];
-int i,l;
-i=0;
-printf("Enter a string: \n");
+char s1[100],s2[100],s3[100];
+int i,j,length;
+i=0,j=0,length;
+printf("Enter string 1:\n");
 gets(s1);
+printf("Enter string 2:\n");
+gets(s2);
 while(s1[i]!='\0')
-{s2[i]=s1[i];
-i++;}
-s2[i]='\0';
-l=0,i=0;
+{s3[j]=s1[i];
+i++;
+j++;}
+i=0;
 while(s2[i]!='\0')
-{l++;
-i++;}
-printf("The copied string is :");
-puts(s2);
-printf("\n The length of the copied string = %d",l);
+{s3[j]=s2[i];
+i++;
+j++;}
+s3[j]='\0';
+i=0,length=0;
+while(s3[i]!='\0')
+{
+    length++;
+    i++;
+}
+printf("The concatenated string is: ");
+puts(s3);
+printf("\n The length of the concatenated string = %d",length);
 }
